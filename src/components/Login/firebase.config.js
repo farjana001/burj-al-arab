7dde20003fa86d32beea8654c8d73e7ae60b7120
Farjana Islam <nipa.farzana87@gmailcon>
2020-09-16 12:35:48 +0600

    done

---
+++ src/components/Login/firebase.config.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDw1dQT5ZQHh32prlpua-sxk2LyWC4VcVs",
+    authDomain: "burj-al-arabs.firebaseapp.com",
+    databaseURL: "https://burj-al-arabs.firebaseio.com",
+    projectId: "burj-al-arabs",
+    storageBucket: "burj-al-arabs.appspot.com",
+    messagingSenderId: "890556564547",
+    appId: "1:890556564547:web:45085bc83c1a32bd0dc0f6"
+  };
+
+  export default firebaseConfig;
